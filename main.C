a9be9275c38073535fd6cf1a8aba33155ad79400
manisaikumarvanka-crypto <manisaikumarvanka@gmail.com>
2025-08-08 18:07:04 +0530

    Update main.C

--- main.C
+++ main.C
@@ -1,10 +1,36 @@
-#include<iostream>
-#include<string.h>
-#include<cstring>
+extern "C"{
+void connectToDB();
+void sendOTP();
+void registerUSER();
+void saveScore();
+void getOverallScore();
+void getWeeklyScore();
+}
+int g_retVal = 0;
+connectToDB();  //Connecting to db 
+string msgType ;
+cin >> msgType;
+switch(msgType) {
+case "SENDOTP" :
+g_retVal = sendOTP();
+break;
+case "REGISTERUSER" :
+g_retVal = registerUSER();
+break;
+case "SAVESCORE" :
+g_retVal = saveScore();
+break;
+case "GETOVERALLSCORE" :
+g_retVal = getOverallScore();
+break;
+case "WEEKLYSCORE" : 
+g_retVal = getWeeklyScore();
+break;
+default :
+cout << " MessgeType is not handled in the system "<<endl;
+break;
+}
+return 0;
+}
 
-using namespace std;
-
-int main(){
-
-connect_to_db();
 
